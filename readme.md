1a8cd106439b81d679bcbd227585443e1acbd5e1
Neel Krishnan <neel@digitalx.com>
2015-10-27 16:53:08 +0000

    Initial commit of sample node.js module for interacting with the DigitalX Direct API

---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a node.js wrapper for accessing the DigitalX Direct bitcoin liquidity platform.
+Please consult test_digitalx.js for usage. 
+Please direct any questions to neel@digitalbtc.com or to request a test account. 
---
+++ test_digitalx.js
@@ -0,0 +1,36 @@
+var Digitalx = require('digitalx');
+
+
+/*var digitalx = new Digitalx(key, secret);*/
+var digitalx = new Digitalx('YOUR API KEY HERE', 'YOUAR API SECRET HERE');
+
+
+
+digitalx.price("buy",
+    function(err, res){
+        console.log(res);
+});
+
+
+digitalx.transaction("d1c72f2b-ce23-5d2d-b38e-92a13f8f94ea",
+    function(err, res){
+        console.log(res);
+});
+
+digitalx.invoice(
+    function(err, res){
+        console.log(res);
+});
+
+
+// digitalx.order(price, amount, 'buy' ... 
+digitalx.order(.001, 400, 'buy',
+    function(err, res){
+        console.log(res);
+});
+
+digitalx.status(
+    function(err, res){
+        console.log(res);
+});
+
